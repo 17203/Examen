ba40755d2fb0eaf311713cd8940dd152ce78c8a6
17203 <17203@portalcedi.edu.mx>
2024-05-25 22:31:31 -0600

    En teoria codigo medio funcional de ronndas :D

--- main.cpp
+++ main.cpp
@@ -1,57 +1,59 @@
 #include <SFML/Graphics.hpp>
-#include "Rrectangle.hpp"
 #include "Rondas.hpp"
+#include "Rrectangle.hpp"
 #include <vector>
+
 using namespace std;
 using namespace sf;
 
-vector <Ronda> rondas;
-vector <Rectangle>rectangulos;
-float speedX=1.f;
-float speedY=1.f;
 int main()
 {
     Ronda ron1;
     RenderWindow window(VideoMode(1200, 1000), "SFML works!");
-    
-    
+
     window.setFramerateLimit(120);
 
-Rectangle rect(Vector2f(60.f,30.f),window);
+    vector<Rectangle> rectangulos;
 
     while (window.isOpen())
     {
         Event event;
         while (window.pollEvent(event))
         {
-            // escucha una vez cuando se presiona el click izquierdo del mouse
             if (event.type == Event::Closed)
                 window.close();
-            if (event.type==Event::MouseButtonPressed){
-                if(event.mouseButton.button== Mouse::Left){
-                    float x=float(50);
-                    float y=float(50);
-                    Rectangle r= Rectangle (Vector2f(x,y),window);
+
+            if (event.type == Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == Mouse::Left)
+                {
+                    float x = float(50);
+                    float y = float(50);
+                    Rectangle r = Rectangle(Vector2f(x, y), window);
                     rectangulos.push_back(r);
                     r.drawTo(window);
                 }
-             if(event.mouseButton.button== Mouse::Right){
-            for(auto &r : rectangulos){
-            r.changeColorIfClicked(Mouse::getPosition(window));
-            }
-            ron1.disparaBala();
-             }
+
+                if (event.mouseButton.button == Mouse::Right)
+                {
+                    for (auto &r : rectangulos)
+                    {
+                        r.changeColorIfClicked(Mouse::getPosition(window));
+                    }
+                    ron1.disparaBala();
+                }
             }
         }
 
-//entra al if si el click iquerido esta presionado
-        if(Mouse::isButtonPressed(Mouse::Left));
-         window.clear();
-        for(auto &r: rectangulos){
+        window.clear();
+        for (auto &r : rectangulos)
+        {
             r.update();
             r.drawTo(window);
         }
+        ron1.drawTo(window);//balas de la ronda actual
         window.display();
+        ron1.update();//reinincia rondnas
     }
 
     return 0;
